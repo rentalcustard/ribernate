0a00403317e705487723f0212c3b35196f665cd9
Tom Stuart <tom@therye.org>
2011-11-02 12:57:26 +0000

    Some comments which might give clues

--- spec/ribernate_spec.rb
+++ spec/ribernate_spec.rb
@@ -2,10 +2,11 @@ require File.dirname(__FILE__) + "/../lib/ribernate"
 class PostDAO
   include Ribernate::DAOMethods
 
-  persists :posts
+  persists :posts #should this be optional?
 end
 
 class Post
+  #we're going to need some data-defining methods here I think.
 end
 
 describe "Ribernate behaviours" do
