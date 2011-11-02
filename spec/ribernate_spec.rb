e0deb23bccc2779cdea6ac5d183c583c85246703
Tom Stuart <tom@therye.org>
2011-11-02 19:28:21 +0000

    Even clearer

--- spec/ribernate_spec.rb
+++ spec/ribernate_spec.rb
@@ -8,10 +8,12 @@ end
 
 class User
   include Ribernate::Model
-  attr_persisted :name
+  attr_persisted :name, :id
 
   def initialize
     @name = "foo"
+    @id = 5
+    @created_at = Time.now
   end
 end
 
@@ -22,10 +24,10 @@ describe "Ribernate behaviours" do
   subject { UserDAO.new }
 
   describe "persisting an object" do
-    it "doesn't explode" do
+    it "sends a query to the database with each persisted attribute, and no others" do
       user = User.new
       subject.save(user)
-      Ribernate::DB.engine.sent_queries.should include('INSERT INTO "users" ("name") VALUES (\'foo\')')
+      Ribernate::DB.engine.sent_queries.should include('INSERT INTO "users" ("name", "id") VALUES (\'foo\', 5)')
     end
   end
 end
